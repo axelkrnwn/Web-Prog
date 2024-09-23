bf9bf08213e6a59fdbcdf8dec4034efab2f13ec9
testing-xx <axelkurn@gmail.com>
2024-09-23 09:53:17 +0700

    Add: Routes list (method get)

--- lawchatapp/routes/web.php
+++ lawchatapp/routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+//landing page
 Route::get('/', function () {
+    return view('welcome');
+});
+
+//login page
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+//register page
+Route::get('/register', function () {
+    return view('welcome');
+});
+
+//chat page
+Route::get('/chat/{id?}', function (string $id="") {
+    echo "hai, '".$id."'";
     return view('welcome');
 });
