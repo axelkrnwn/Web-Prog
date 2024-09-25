13f66f053eafcbb83b119bee6940c590f33b586d
testing-xx <axelkurn@gmail.com>
2024-09-25 13:21:01 +0700

    Add: controllers + post routes

--- lawchatapp/routes/web.php
+++ lawchatapp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 //landing page
@@ -12,13 +13,26 @@ Route::get('/login', function () {
     return view('welcome');
 });
 
+Route::post('/login', function (Request $request) {
+    return view('welcome');
+});
+
 //register page
 Route::get('/register', function () {
     return view('welcome');
 });
 
+Route::post('/register', function (Request $request) {
+    return view('welcome');
+});
+
 //chat page
 Route::get('/chat/{id?}', function (string $id="") {
+    echo "hai, '".$id."'";
+    return view('welcome');
+});
+
+Route::post('/chat/{id?}', function (string $id="", Request $request) {
     echo "hai, '".$id."'";
     return view('welcome');
 });
