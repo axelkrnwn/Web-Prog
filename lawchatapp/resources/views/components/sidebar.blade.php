87e22a6c7c7b86380277e55be0b0db0d8ebf14d7
testing-xx <axelkurn@gmail.com>
2024-12-18 22:07:10 +0700

    Refactor: chat history

--- lawchatapp/resources/views/components/sidebar.blade.php
+++ lawchatapp/resources/views/components/sidebar.blade.php
@@ -1,9 +1,5 @@
 <div class="p-3" style="display: flex; flex-direction: column; row-gap: 10px">
     <div class="d-flex">
-        <form action="/logout" method="post">
-            @csrf
-            <button class="btn btn-danger">Logout</button>
-        </form>
         <a href="/chat/">
             <button class="btn btn-info">Add New Chat</button>
         </a>
--- lawchatapp/resources/views/home/chatroom.blade.php
+++ lawchatapp/resources/views/home/chatroom.blade.php
@@ -10,58 +10,68 @@
     <link rel="stylesheet" href="{{ asset('styles/main.css') }}">
 </head>
 <body style="background-color: #f5f5f5 w-100 h-100">
-    <div>
-        <div class="d-flex my-2 flex-col align-items-center justify-content-between w-100">
-            <div class="menu-button">
-                <div class="rectangle"><span></span></div>
-                <div class="rectangle"><span></span></div>
-                <div class="rectangle"><span></span></div>
-            </div>
-            <h1 class="title">HukumKU Bot</h1>
-            <form action="/logout" method="post" class="ms-auto">
-                @csrf
-                <button type="submit" class="logout-btn">
-                    <img src="{{ asset('assets/logout_icon.png') }}" alt="Logout">
-                </button>
-            </form>
+    <div class="d-flex w-100">
+        <div style="width: 40%; height: 100vh; display: none" id="sidebar">
+            <x-sidebar></x-sidebar>
         </div>
-    </div>
-        
-    <div style="position: relative; height: 80%; width: 100%; box-sizing: border-box">
-        <div class="px-5 mx-5" style="overflow-y: scroll; height: 100%;">
-            @isset($chats)
-                @foreach ($chats as $chat)
-                    @if ($chat->is_generated == 0)
-                        <div class="d-flex justify-content-end">
-                            <div class="user-chat py-2">
-                                <p class="px-3 h6">{{$chat->content}}</p>
-                            </div>
-                        </div>
-                    @else
-                        <div class="d-flex justify-content-start my-2">
-                            <div class="bot-chat py-2">
-                                <p class="px-3 h6">{{$chat->content}}</p>
-                            </div>
-                        </div>
-                    @endif
-                @endforeach
-            @endisset
+        <div class="w-100" style="height: 85vh">
+            <div class="w-100">
+                <div class="d-flex my-2 flex-col align-items-center justify-content-between w-100">
+                    <div class="menu-button" id="menu-button">
+                        <div class="rectangle"><span></span></div>
+                        <div class="rectangle"><span></span></div>
+                        <div class="rectangle"><span></span></div>
+                    </div>
+                    <h1 class="title">HukumKU Bot</h1>
+                    <form action="/logout" method="post" class="ms-auto">
+                        @csrf
+                        <button type="submit" class="logout-btn">
+                            <img src="{{ asset('assets/logout_icon.png') }}" alt="Logout">
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <div style="position: relative; height: 80%; width: 100%; box-sizing: border-box">
+                <div class="px-5 mx-5" style="overflow-y: scroll; height: 100%;">
+                    @isset($chats)
+                        @foreach ($chats as $chat)
+                            @if ($chat->is_generated == 0)
+                                <div class="d-flex justify-content-end">
+                                    <div class="user-chat py-2" id = "chat">
+                                        <input class="px-3 h6 w-100" style="border: none; background: none;" value="{{$chat->content}}"/>
+                                    </div>
+                                </div>
+                            @else
+                                <div class="d-flex justify-content-start my-2">
+                                    <div class="bot-chat py-2">
+                                        <p class="px-3 h6">{{$chat->content}}</p>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    @endisset
+                </div>
+
+            <div class="px-5 py-5 flex justify-content-center absolute-bottom">
+                <form class="prompt px-3 mx-5" method="POST" action={{"/chat/".$id}}>
+                    @csrf
+                    <input type="text" placeholder="Message HukumKU Bot" name="content" class="bg-transparent border-0 text-black w-100">
+                    <button type="submit" class="bg-transparent border-0 prompt-button">
+                        <img src="{{ asset('assets/arrow-right.png') }}" alt="" style="width: 90%; height: 100%;">
+                    </button>
+                </form>
+            </div>
+
+        </div>
         </div>
 
-    <div class="px-5 py-5 flex justify-content-center fixed-bottom">
-        <form class="prompt px-3 mx-5" method="POST" action="/chat">
-            @csrf
-            <input type="text" placeholder="Message HukumKU Bot" class="bg-transparent border-0 text-black w-100">
-            <button type="submit" class="bg-transparent border-0 prompt-button">
-                <img src="{{ asset('assets/arrow-right.png') }}" alt="" style="width: 90%; height: 100%;">
-            </button>
-        </form>
-    </div>
-    
+
 </body>
 <script>
     document.getElementById('menu-button').addEventListener('click', () => {
         document.getElementById('sidebar').style.display = document.getElementById('sidebar').style.display == 'none'?'block':'none'
     })
+    document.getElementById('')
 </script>
 </html>
