b6032f617d0311aea851f4aa44b154aad70a55a4
Frederick <frederickchandra52@gmail.com>
2024-12-18 18:01:46 +0700

    fix ui

--- lawchatapp/resources/views/home/chatroom.blade.php
+++ lawchatapp/resources/views/home/chatroom.blade.php
@@ -9,76 +9,45 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('styles/main.css') }}">
 </head>
-<<<<<<< Updated upstream
-<body style="background-color: #f5f5f5; height: 100vh; display: flex; widows: 100vw;">
-    <div id = "sidebar" style="display: none; width: 30%; background-color: #CBB9B9; height: 100vh;">
-        <x-sidebar></x-sidebar>
-=======
-<body style="background-color: #f5f5f5">
-    <div class="p-2">
-    <div class="d-flex my-2 flex-col align-items-center justify-content-between w-100">
-        <div class="menu-button">
-            <div class="rectangle"><span></span></div>
-            <div class="rectangle"><span></span></div>
-            <div class="rectangle"><span></span></div>
+<body style="background-color: #f5f5f5 w-100 h-100">
+    <div>
+        <div class="d-flex my-2 flex-col align-items-center justify-content-between w-100">
+            <div class="menu-button">
+                <div class="rectangle"><span></span></div>
+                <div class="rectangle"><span></span></div>
+                <div class="rectangle"><span></span></div>
+            </div>
+            <h1 class="title">HukumKU Bot</h1>
+            <form action="/logout" method="post" class="ms-auto">
+                @csrf
+                <button type="submit" class="logout-btn">
+                    <img src="{{ asset('assets/logout_icon.png') }}" alt="Logout">
+                </button>
+            </form>
         </div>
-        <h1 class="title">HukumKU Bot</h1>
-        <form action="/logout" method="post" class="ms-auto">
-            @csrf
-            <button type="submit" class="logout-btn">
-                <img src="{{ asset('assets/logout_icon.png') }}" alt="Logout">
-            </button>
-        </form>
->>>>>>> Stashed changes
     </div>
-    <div class="pb-2" style="width: 100%">
-        <div class="p-2">
-            <div class="d-flex flex-col align-items-center justify-content-between w-25">
-                <div class="menu-button" id = "menu-button">
-                    <div class="rectangle"><span></span></div>
-                    <div class="rectangle"><span></span></div>
-                    <div class="rectangle"><span></span></div>
-                </div>
-                <h4 class="title">HukumKU Bot</h4>
-            </div>
-
+        
+    <div style="position: relative; height: 80%; width: 100%; box-sizing: border-box">
+        <div class="px-5 mx-5" style="overflow-y: scroll; height: 100%;">
+            @isset($chats)
+                @foreach ($chats as $chat)
+                    @if ($chat->is_generated == 0)
+                        <div class="d-flex justify-content-end">
+                            <div class="user-chat py-2">
+                                <p class="px-3 h6">{{$chat->content}}</p>
+                            </div>
+                        </div>
+                    @else
+                        <div class="d-flex justify-content-start my-2">
+                            <div class="bot-chat py-2">
+                                <p class="px-3 h6">{{$chat->content}}</p>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            @endisset
         </div>
 
-
-        <div style="position: relative; height: 80%; width: 100%; box-sizing: border-box">
-            <div class="px-5 mx-5" style="overflow-y: scroll; height: 100%;">
-                @isset($chats)
-                    @foreach ($chats as $chat)
-                        @if ($chat->is_generated == 0)
-                            <div class="d-flex justify-content-end">
-                                <div class="user-chat py-2">
-                                    <p class="px-3 h6">{{$chat->content}}</p>
-                                </div>
-                            </div>
-                        @else
-                            <div class="d-flex justify-content-start my-2">
-                                <div class="bot-chat py-2">
-                                    <p class="px-3 h6">{{$chat->content}}</p>
-                                </div>
-                            </div>
-                        @endif
-                    @endforeach
-                @endisset
-            </div>
-
-<<<<<<< Updated upstream
-
-            <div class="px-5 flex justify-content-center absolute-bottom">
-                <form class="prompt px-3 mx-5" method="POST" action="/chat/{{$id}}">
-                    @csrf
-                    <input type="text" placeholder="Message HukumKU Bot" class="bg-transparent border-0 text-black w-100" name="content">
-                    <button type="submit" class="bg-transparent border-0 prompt-button">
-                        <img src={{ asset('assets/arrow-right.png') }} alt="" style="width: 100%; height: 100%;">
-                    </button>
-                </form>
-            </div>
-        </div>
-=======
     <div class="px-5 py-5 flex justify-content-center fixed-bottom">
         <form class="prompt px-3 mx-5" method="POST" action="/chat">
             @csrf
@@ -87,7 +56,6 @@
                 <img src="{{ asset('assets/arrow-right.png') }}" alt="" style="width: 90%; height: 100%;">
             </button>
         </form>
->>>>>>> Stashed changes
     </div>
     
 </body>
